721a0a24fdddc1fd9049a2bbc5cf1d05a4378888
pankaj1610 <pankajsha584@gmail.com>
2021-01-03 13:40:57 +0530

    Added GetSubSequences.java

--- Recursion and Backtracking/Recursion with ArrayList/GetSubSequences.java
+++ Recursion and Backtracking/Recursion with ArrayList/GetSubSequences.java
@@ -17,11 +17,11 @@ abc
 Sample Output
 [, c, b, bc, a, ac, ab, abc]
 */
-
+/*
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class GetSubSequences {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
@@ -50,24 +50,7 @@ public class Main {
         return myres;//returning myres
     }
 
-}/*
-Question
-1. You are given a string str.
-2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
-Use sample input and output to take idea about subsequences.
-
-Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.
-Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
-Input Format
-A string str
-Output Format
-Contents of the arraylist containing subsequences as shown in sample output
-Constraints
-0 <= str.length <= 20
-Sample Input
-abc
-Sample Output
-[, c, b, bc, a, ac, ab, abc]
+}
 */
 
 import java.io.*;
